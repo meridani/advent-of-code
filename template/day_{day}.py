b320157f74218c0540e2ba388fca8ac08163560f
Haszmann Daniel (AE/EEC6) <Daniel.Haszmann@hu.bosch.com>
2022-12-07 17:29:13 +0100

    Update test and templates

---
+++ template/day_{day}.py
@@ -0,0 +1,30 @@
+import aoc_helper
+from aoc_helper import (Grid, PrioQueue, decode_text, extract_ints, frange,
+                        irange, iter, list, map, range, tail_call)
+
+
+def parse_raw(raw):
+    return raw
+
+def part_one():
+    ...
+
+
+def part_two():
+    ...
+
+def test_ex1():
+    raw=""""""
+    global data
+    data = parse_raw(raw)
+    assert(part_one()==0)
+    assert(part_two()==0)
+
+if __name__ == "__main__":
+    raw = aoc_helper.fetch({day}, {year})
+    data = parse_raw(raw)
+    try:
+        aoc_helper.lazy_submit(day={day}, year={year}, solution=part_one)
+        aoc_helper.lazy_submit(day={day}, year={year}, solution=part_two)
+    except:
+        print("Can't upload to AoC")
