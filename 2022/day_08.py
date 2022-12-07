ce1d5fcf52e1d64db24c7e98f79f0c15ef997e96
Haszmann Daniel (AE/EEC6) <Daniel.Haszmann@hu.bosch.com>
2022-12-07 17:29:48 +0100

    Day 8

--- 2022/day_08.py
+++ 2022/day_08.py
@@ -1,40 +1,30 @@
-from typing import List
-
 import aoc_helper
 from aoc_helper import (Grid, PrioQueue, decode_text, extract_ints, frange,
                         irange, iter, list, map, range, tail_call)
 
-raw = aoc_helper.fetch(8, 2022)
 
-
-def parse_raw() -> List:
-    pass
-    # return [line.split(" ") for line in raw.splitlines()]
-
-data: List = parse_raw()
+def parse_raw(raw):
+    return raw
 
 def part_one():
-    pass
+    ...
+
 
 def part_two():
-    pass
+    ...
 
 def test_ex1():
-    global raw
-    global data
     raw=""""""
-    data = parse_raw()
-    assert(part_one()==1)
-    assert(part_two()==1)
+    global data
+    data = parse_raw(raw)
+    assert(part_one()==0)
+    assert(part_two()==0)
 
 if __name__ == "__main__":
-    # test_ex1()
-    p1 = part_one()
-    p2 = part_two()
-    print(p1)
-    print(p2)
+    raw = aoc_helper.fetch(8, 2022)
+    data = parse_raw(raw)
     try:
-        aoc_helper.lazy_submit(day=8, year=2022, solution=p1)
-        aoc_helper.lazy_submit(day=8, year=2022, solution=p2)
+        aoc_helper.lazy_submit(day=8, year=2022, solution=part_one)
+        aoc_helper.lazy_submit(day=8, year=2022, solution=part_two)
     except:
         print("Can't upload to AoC")
