7ff75b4b95539e1b4538f358acf0d9ea40194c18
Haszmann Daniel (AE/EEC6) <Daniel.Haszmann@hu.bosch.com>
2022-12-12 19:33:12 +0100

    Fix Day 1 if last elf is in the top 3

--- 2022/day_01.py
+++ 2022/day_01.py
@@ -6,7 +6,7 @@ raw: str = aoc_helper.fetch(1, 2022)
 
 
 def parse_raw():
-    return raw.splitlines()
+    return raw.split("\n\n")
 
 
 data = parse_raw()
@@ -14,16 +14,9 @@ data = parse_raw()
 elves = []
 
 def part_one():
-    biggest = 0
-    tempsum = 0
-    for item in data:
-        if item != "":
-            tempsum = tempsum + int(item)
-        else:
-            if tempsum > biggest:
-                biggest = tempsum
-            elves.append(tempsum)
-            tempsum = 0
+    for elf in data:
+        items = list(map(int,elf.split()))
+        elves.append(sum(items))
     return max(elves)
 
 
@@ -31,6 +24,27 @@ def part_two():
     elves.sort()
     return sum(elves[-3:])
 
+
+def test_ex1():
+    global raw
+    raw="""1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+    global data
+    data = parse_raw()
+    assert(part_one()==24000)
+    assert(part_two()==45000)
 
 if __name__ == "__main__":
     aoc_helper.lazy_submit(day=1, year=2022, solution=part_one)
