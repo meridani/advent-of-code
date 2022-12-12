b3e8b8531dea3f36f38e1a24d5d2966760ca231f
meridani <szmanndani@gmail.com>
2022-12-12 19:20:24 +0100

    Prepare day 13

---
+++ 2022/day_13.py
@@ -0,0 +1,30 @@
+import aoc_helper
+from aoc_helper import (Grid, PrioQueue, decode_text, extract_ints, frange,
+                        irange, iter, list, map, range, tail_call)
+
+
+def parse_raw(raw):
+    return raw
+
+def part_one():
+    ...
+
+
+def part_two():
+    ...
+
+def test_ex1():
+    raw=""""""
+    global data
+    data = parse_raw(raw)
+    assert(part_one()==0)
+    assert(part_two()==0)
+
+if __name__ == "__main__":
+    raw = aoc_helper.fetch(13, 2022)
+    data = parse_raw(raw)
+    try:
+        aoc_helper.lazy_submit(day=13, year=2022, solution=part_one)
+        aoc_helper.lazy_submit(day=13, year=2022, solution=part_two)
+    except Exception as err:
+        print("Can't upload to AoC", err)
