0a832ad7d5895b92c5fb1ce0681870068a2511fc
meridani <szmanndani@gmail.com>
2023-12-01 09:08:06 +0100

    Day 1 ok

--- 2023/day_01.py
+++ 2023/day_01.py
@@ -1,3 +1,4 @@
+import re
 from collections import defaultdict, deque
 from typing import TYPE_CHECKING
 
@@ -23,9 +24,31 @@ from aoc_helper import (
 
 raw = aoc_helper.fetch(1, 2023)
 
+digits = {
+    "one": "one1one",
+    "two": "two2two",
+    "three": "three3three",
+    "four": "four4four",
+    "five": "five5five",
+    "six": "six6six",
+    "seven": "seven7seven",
+    "eight": "eight8eight",
+    "nine": "nine9nine",
+}
+
 
 def parse_raw(raw: str):
-    return ...
+    nr = []
+    for line in raw.splitlines():
+        # only for part2
+        for num, val in digits.items():
+            line = line.replace(num, val)
+
+        n = re.findall(r"(\d)", line)
+
+        if len(n) > 0:
+            nr.append(int(str(n[0]) + str(n[-1])))
+    return nr
 
 
 data = parse_raw(raw)
@@ -35,7 +58,7 @@ data = parse_raw(raw)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    return sum(data)
 
 
 aoc_helper.lazy_test(day=1, year=2023, parse=parse_raw, solution=part_one)
@@ -45,7 +68,7 @@ aoc_helper.lazy_test(day=1, year=2023, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    return sum(data)
 
 
 aoc_helper.lazy_test(day=1, year=2023, parse=parse_raw, solution=part_two)
