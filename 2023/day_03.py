c1c0e8a06ccb2795437a46d590d5e38c8eaed2f6
meridani <szmanndani@gmail.com>
2023-12-03 11:53:49 +0100

    Day3 part2

--- 2023/day_03.py
+++ 2023/day_03.py
@@ -1,13 +1,15 @@
+import itertools
+import re
 from collections import defaultdict, deque
 from math import prod
 from typing import TYPE_CHECKING
 
 import aoc_helper
+from aoc_helper import decode_text  # list,
 from aoc_helper import (
     Grid,
     PrioQueue,
     SparseGrid,
-    decode_text,
     extract_ints,
     extract_iranges,
     extract_ranges,
@@ -15,7 +17,6 @@ from aoc_helper import (
     frange,
     irange,
     iter,
-    list,
     map,
     range,
     search,
@@ -30,8 +31,9 @@ def classifier(char: str, /) -> int:
 
 
 def parse_raw(raw: str):
-    grid = Grid("").from_string(raw, classifier)
-    return grid
+    # grid = Grid("").from_string(raw, classifier)
+    # return grid
+    return raw
 
 
 data = parse_raw(raw)
@@ -78,35 +80,60 @@ aoc_helper.lazy_test(day=3, year=2023, parse=parse_raw, solution=part_one)
 # collections (list, set, dict, tuple)
 def part_two(data=data):
     sums = 0
-    for y, row in enumerate(data):
-        for x, char in enumerate(row):
-            if char == "*":
-                nr_list = []
-                n = data.neighbours(y, x)
-                for loc, _ in n:
-                    y_ = loc[0]
-                    x_ = loc[1]
-                    if data[y_][x_] in "0123456789":
-                        while x_ != 0:
-                            if data[y_][x_ - 1] in "0123456789":
-                                x_ -= 1
-                            else:
-                                break
+    box = list(itertools.product((-1, 0, 1), (-1, 0, 1)))
+    symbols = {
+        (x, y)
+        for y, line in enumerate(data.splitlines())
+        for x, c in enumerate(line)
+        if not c.isdigit() and c != "."
+    }
 
-                        nr_list.append((x_, y_))
-                nr_list = list(set(nr_list))
-                if len(nr_list) == 2:
-                    nums = []
-                    for x_, y_ in nr_list:
-                        num = ""
-                        while data[y_][x_] in "0123456789":
-                            num += data[y_][x_]
-                            x_ += 1
-                            if x_ >= len(data[y_]):
-                                nums.append(int(num))
-                                break
-                        nums.append(int(num))
-                    sums += prod(nums)
+    gears = defaultdict(list)
+
+    for y, line in enumerate(data.splitlines()):
+        for match in re.finditer(r"\d+", line):
+            neighbours = {
+                (x + dx, y + dy)
+                for dx, dy in box
+                for x in range(match.start(), match.end())
+            }
+            # print(neighbours)
+            for symbol in symbols & neighbours:
+                if data.splitlines()[symbol[1]][symbol[0]] == "*":
+                    gears[symbol].append(int(match.group(0)))
+
+    for gear in gears.values():
+        if len(gear) == 2:
+            sums += prod(gear)
+    # for y, row in enumerate(data):
+    #     for x, char in enumerate(row):
+    #         if char == "*":
+    #             nr_list = []
+    #             n = data.neighbours(y, x)
+    #             for loc, _ in n:
+    #                 y_ = loc[0]
+    #                 x_ = loc[1]
+    #                 if data[y_][x_] in "0123456789":
+    #                     while x_ != 0:
+    #                         if data[y_][x_ - 1] in "0123456789":
+    #                             x_ -= 1
+    #                         else:
+    #                             break
+
+    #                     nr_list.append((x_, y_))
+    #             nr_list = list(set(nr_list))
+    #             if len(nr_list) == 2:
+    #                 nums = []
+    #                 for x_, y_ in nr_list:
+    #                     num = ""
+    #                     while data[y_][x_] in "0123456789":
+    #                         num += data[y_][x_]
+    #                         x_ += 1
+    #                         if x_ >= len(data[y_]):
+    #                             nums.append(int(num))
+    #                             break
+    #                     nums.append(int(num))
+    #                 sums += prod(nums)
     return sums
 
 
